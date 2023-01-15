1ae70c7cd6499ab662e0767e61542863659f8512
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-14 22:48:44 -0600

    Feature: Added canvas manipulation from Rust
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
@@ -33,4 +34,24 @@ pub fn greet() {
 #[wasm_bindgen(start)]
 pub fn run() {
     log("Hello, World!");
+
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+    context.move_to(0.0, 0.0);
+    context.line_to(100.0, 100.0);
+    context.stroke();
 }
