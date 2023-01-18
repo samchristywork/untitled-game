28028217bff42115218ec403479ba2d0e63f36e3
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-17 21:39:40 -0600

    Feature: Added simple sprite loading

--- render.js
+++ render.js
@@ -1,6 +1,10 @@
 export function render(counter) {
   let canvas=document.querySelector("#canvas");
   let context=canvas.getContext("2d");
+
+  var img = document.getElementById("sprites");
+  context.drawImage(img, counter, 10);
+
   context.rect(counter, 0, 10, 10);
   context.stroke();
 }
