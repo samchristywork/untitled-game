944850c0f7eb0e258ae7341f425d624f62851a50
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-17 22:28:05 -0600

    Feature: Added keyboard events

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let canvas=document.querySelector("#canvas");
+let debug=document.querySelector("#debug");
+console.log(canvas);
+
+var pressed={};
+canvas.addEventListener("keydown", function(e) {
+  pressed[e.keyCode] = true;
+  debug.innerHTML=JSON.stringify(pressed);
+});
+
+canvas.addEventListener("keyup", function(e) {
+  e = e || window.event;
+  delete pressed[e.keyCode];
+  debug.innerHTML=JSON.stringify(pressed);
+});
+
 import init, { status } from "./pkg/untitled_game.js";
 init().then(() => {
   status();
