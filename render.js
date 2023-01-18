6674445ff8aab3ed621554d754c79a30ec37af06
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-17 21:42:28 -0600

    Feature: Only take a portion of the sprite sheet

--- render.js
+++ render.js
@@ -5,6 +5,16 @@ export function render(counter) {
   var img = document.getElementById("sprites");
   context.drawImage(img, counter, 10);
 
+  let sx=0;
+  let sy=0;
+  let swidth=32;
+  let sheight=32;
+  let dx=counter;
+  let dy=64;
+  let dwidth=32;
+  let dheight=32;
+  context.drawImage(img, sx, sy, swidth, sheight, dx, dy, dwidth, dheight);
+
   context.rect(counter, 0, 10, 10);
   context.stroke();
 }
