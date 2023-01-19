800cb64410246f7e3947168d15303b14da26bfbc
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-18 20:25:25 -0600

    Feature: Render a solitary 16x16 sprite

--- render.js
+++ render.js
@@ -6,23 +6,17 @@ export function render(x, y) {
 
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  var img = document.getElementById("sprites");
-  context.drawImage(img, x, 10);
+  var img = document.getElementById("sprites");
 
-  let sx=0;
+  let sx=16;
   let sy=0;
-  let swidth=32;
-  let sheight=32;
+  let swidth=16;
+  let sheight=16;
   let dx=x;
-  let dy=y+64;
-  let dwidth=32;
-  let dheight=32;
-  context.drawImage(img, sx, sy, swidth, sheight, dx, dy, dwidth, dheight);
-
-  context.drawImage(img, sx+32, sy, swidth, sheight, dx, dy+32, dwidth, dheight);
-
-  context.rect(x, 0, 10, 10);
-  context.stroke();
+  let dy=y;
+  let dwidth=16;
+  let dheight=16;
+  context.drawImage(img, sx, sy, swidth, sheight, dx, dy, dwidth, dheight);
 
   return debug.innerHTML;
 }
