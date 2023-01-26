51e3bd4609e1122eb3bf6a2d4195400e282c7067
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-25 21:15:00 -0600

    Refactor: src/attribute.rs refactored into a file

---
+++ src/attribute.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, PartialEq)]
+pub enum Attribute {
+    Blocking,
+    Consumable,
+    Consumed,
+    Controllable,
+    Dynamic,
+    Harmful,
+    Hastened,
+    Healing,
+    Invisible,
+    Moving,
+    Player,
+    Slowed,
+}
