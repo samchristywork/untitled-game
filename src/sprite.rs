9cf4a4ae09f6ffd9827cca0bfbc9cf97b8885d64
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-25 21:15:28 -0600

    Refactor: src/sprite.rs refactored into a file

---
+++ src/sprite.rs
@@ -0,0 +1,33 @@
+use crate::attribute::Attribute;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Sprite {
+    pub name: String,
+    pub x: i32,
+    pub y: i32,
+    pub rotation: i32,
+    pub scale: f32,
+    pub idx: u32,
+    pub attributes: Vec<Attribute>,
+    pub show_debug: bool,
+    pub flip: bool,
+    pub invisible: bool,
+}
+
+fn dist_squared(x1: i32, y1: i32, x2: i32, y2: i32) -> i32 {
+    let dx = x1 - x2;
+    let dy = y1 - y2;
+
+    dx * dx + dy * dy
+}
+
+impl Sprite {
+    pub fn collides_with(&self, s: &Sprite) -> bool {
+        if dist_squared(self.x, self.y, s.x, s.y) < 100 {
+            true
+        } else {
+            false
+        }
+    }
+}
